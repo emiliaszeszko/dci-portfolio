56ed12a1d085f905b7564c21d54d068779285f0c
Emilia <emilia.szeszko@aya.yale.edu>
2018-01-24 12:49:49 -0500

    adding functions practice

---
+++ week-4/functions-practice.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace functions_practice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Pick a letter, any letter:");
+            char x = Convert.ToChar(Console.ReadLine());
+
+            Console.WriteLine(VowelTest(Char.ToLower(x)));
+
+            Console.WriteLine("Pick a number 1-10:");
+            int a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Pick a second number 1-10:");
+            int b = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Pick a third number 1-10");
+            int c = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine(ReturnLargestInt(a,b,c));
+        }
+
+        static bool VowelTest(char letter)
+        {
+            if (letter == 'a' || letter == 'e' || letter == 'i' || letter == 'o' || letter == 'u')
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        static int ReturnLargestInt(int num1, int num2, int num3)
+        {
+            if (num1 >= num2 && num1 >= num3)
+            {
+                return num1;
+            }
+            else if (num2 >= num3)
+            {
+                return num2;
+            }
+            else
+            {
+                return num3;
+            }
+        }
+    }
+}
