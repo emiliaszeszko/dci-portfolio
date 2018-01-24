9deef102dded8c79a17cbb258f49ed37dd29ae2f
Emilia <emilia.szeszko@aya.yale.edu>
2018-01-24 09:41:25 -0500

    added files from week 3
    C# practice with loops

---
+++ week-3/loops.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace csharp_practice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        //while loop
+        {
+            int i = 1;
+            while (i <= 5)
+            {
+                Console.WriteLine("The value of i = {0}", i);
+                i++;
+            }
+        }
+        //do while loop
+        private static void DoWhileTest()
+        {
+            int i = 1;
+            do
+            {
+                Console.WriteLine("The value of i = {0}", i);
+                i++;
+            }
+            while (i <= 5);
+        }
+        //for loop
+        private static void ForTest()
+        {
+            for (int i = 1; i <= 5; i++)
+            {
+                Console.WriteLine("The value of i = {0}", i);
+            }
+        }
+    }
+}
---
+++ week-4/assignment-2.cs
@@ -0,0 +1,71 @@
+﻿using System;
+
+namespace assignment_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //2.1 Write a program in C# to make a pattern like a right triangle with the number increased by 1//
+            //initial try - literal print of numbers 1-10//
+            /*int num = 1;
+            while(num < 2)
+            {
+                Console.WriteLine(num);
+                num++;
+            }
+            while(num < 4)
+            {
+                Console.Write(num);
+                num++;
+                Console.WriteLine(num);
+                num++;
+            }
+            while(num < 7)
+            {
+                Console.Write(num);
+                num++;
+                Console.Write(num);
+                num++;
+                Console.WriteLine(num);
+                num++;
+            }
+            while(num < 11)
+            {
+                Console.Write(num);
+                num++;
+                Console.Write(num);
+                num++;
+                Console.Write(num);
+                num++;
+                Console.WriteLine(num);
+                num++;
+            }*/
+
+            //now want to solve this for any numbers not just those specified - DRY code//
+            int value = 1;
+            //n will be the number of lines returned in the output//
+            int n = 4;
+            for (int i = 1; i <= n; i++)
+            {
+                for (int j = 1; j <= i; j++)
+                {
+                    Console.Write(value);
+                    value++;
+                }
+                Console.Write("\n");
+            }
+
+            //2.2 Write a C# program to calculate a factorial of a given number//
+            int m;
+            Console.WriteLine("Enter the number for which you would like to calculate the factorial:");
+            m = Convert.ToInt32(Console.ReadLine());
+
+            for (int k = m-1; k >= 1; k--)
+            {
+                m = m * k;
+            }
+            Console.WriteLine(m);
+        }
+    }
+}
