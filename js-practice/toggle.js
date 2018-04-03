c082466a79355145865b204810e730372eb88a61
Emilia <emilia.szeszko@aya.yale.edu>
2018-04-03 14:20:47 -0400

    toggle js practice
    html and js files to toggle dis/appear

---
+++ js-practice/toggle.js
@@ -0,0 +1,9 @@
+function toggleFunction() {
+  var x = document.getElementById("toggleDiv");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  }
+  else {
+    x.style.display = "none";
+  }
+}
